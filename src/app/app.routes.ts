ff1242d349f2ea4bce350861ee9f13285617ea83
cbisac <cortesbuitragoisac@gmail.com>
2025-05-03 22:51:00 -0500

    feat: create post page UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PostComponent } from './pages/post/post.component';
 export const routes: Routes = [{
     title: 'Home',
     path: '',
@@ -16,8 +17,14 @@ export const routes: Routes = [{
     title: 'register',
     path: 'register',
     component: RegisterComponent
-}, {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
+}
+// , {
+//     path: '**',
+//     redirectTo: '',
+//     pathMatch: 'full'
+// }
+, {
+    title: 'post',
+    path: 'post',
+    component: PostComponent
 }];
